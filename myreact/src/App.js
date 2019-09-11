b170ef8320e5328ffb17c80b63085d97185f6bbb
huangke <55093865+huangke@users.noreply.github.com>
2019-09-11 10:17:04 +0800

    Pagination

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
-import './App.css';
-import Item from './component/Item'
-import axios from "axios"
-import "./mock/index"
+import "./App.css"
+import { Pagination } from 'antd';
+import PageContext from "./component/PageContext"
+
 export default class App extends Component {
-  state={list:[]}
-  componentDidMount(){
-    axios.get("/list").then(({data})=>{
-      this.setState({list:data.list})
-    })
+  state={current:5,pagesize:10,total:200}
+  onShowSizeChange(current, size){
+    console.log(current, size)
+  }
+  onChange=current=>{
+    console.log(current)
+    this.setState({current})
   }
   render() {
-    let {list}=this.state
+    let {current,pagesize,total}=this.state
     return (
       <div className="app">
-        <h5 onClick={()=>{this.componentDidMount()}}>换一换</h5>
-        {list.length&&list.map((item,index)=><Item key={index} item={item} index={index}/>)} 
+          <div className="pagecontext">
+              <PageContext current={current} pagesize={pagesize} total={total}></PageContext>
+          </div>
+          <div className="pagination">
+              <Pagination current={current} pageSize={pagesize} onChange={this.onChange} total={total}/>
+          </div>
       </div>
     )
   }
--- myreact/src/component/Item.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Item extends Component {
-  render() {
-    return (
-      <div className="item"><p><b style={{background:this.props.item.color}}>{this.props.index+1}</b> <span>{this.props.item.title}</span></p><span>{this.props.item.views}万</span></div>
-    )
-  }
-}
